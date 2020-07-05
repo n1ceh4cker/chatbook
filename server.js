8e37892ff955cfe85008992ea2f4367db6a2833f
Nice Akhtar <niceakhtar21@gmail.com>
2020-07-05 16:11:30 +0530

    view engine ejs and static folder added

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
+
+app.use(express.static('public'))
+app.set('view-engine', 'ejs')
 app.get('/', (req, res) =>{
-    res.send('Hello! welcome to chatbook website')
+    res.render('index.ejs')
 })
 server.listen(3000, () =>{
     console.log('server is listening on port 3000...')
