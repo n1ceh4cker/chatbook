9408875a270b91ef6364d44e9fb70df656395bf8
Nice Akhtar <niceakhtar21@gmail.com>
2020-07-05 16:34:30 +0530

    mongodb connected through mongoose

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongodb:{
+        url_string:'mongodb://localhost:27017/chatbook'
+    }
+}
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 const router = require('./router')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 
 //telling express to server static files from folder public
 app.use(express.static('public'))
@@ -10,6 +12,18 @@ app.use(express.static('public'))
 app.set('view-engine', 'ejs')
 //telling express to use router for routes starting at /
 app.use('/', router)
+
+//mongoose database connection
+mongoose.connect(keys.mongodb.url_string, 
+	{ useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+	if(err){
+		console.log(err)
+	}
+	else{
+		console.log('successfully connected to db...')
+	}
+})
+
 server.listen(3000, () =>{
     console.log('server is listening on port 3000...')
 })
