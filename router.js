c9c463522e5a959e54e1460c41944e0acc0a6d4b
Nice Akhtar <niceakhtar21@gmail.com>
2020-07-05 16:25:47 +0530

    controller added

--- router.js
+++ router.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('./controller/userController')
 
-router.get('/', (req, res) =>{
-    res.render('index.ejs')
-})
+router.get('/', userController.home)
 module.exports = router
