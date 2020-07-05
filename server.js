9085ea2c15b4b545c95985f38039c63e5eaf53dc
Nice Akhtar <niceakhtar21@gmail.com>
2020-07-05 16:19:33 +0530

    router added

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
+const router = require('./router')
 
+//telling express to server static files from folder public
 app.use(express.static('public'))
+//telling express to use view engine ejs
 app.set('view-engine', 'ejs')
-app.get('/', (req, res) =>{
-    res.render('index.ejs')
-})
+//telling express to use router for routes starting at /
+app.use('/', router)
 server.listen(3000, () =>{
     console.log('server is listening on port 3000...')
 })
